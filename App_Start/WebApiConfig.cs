eb8440f1971219a23d1a7ea296ec0b67f0e95db5
AshokKumG <160823845+AshokKumG@users.noreply.github.com>
2025-03-20 12:32:25 +0530

    Add files via upload

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using RevCode_API.ActionFilters;
+namespace RevCode_API
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            //var corsAttr = new EnableCorsAttribute("*", "*", "*");
+
+            var corsAttr = new EnableCorsAttribute("*",
+                                          "Origin, Content-Type, Accept",
+                                          "GET, PUT, POST, DELETE, OPTIONS");
+            config.EnableCors(corsAttr);
+            // Web API routes
+            //config.MapHttpAttributeRoutes();
+
+            config.MapHttpAttributeRoutes();
+         //   config.Filters.Add(new LoggingFilterAttribute());
+            config.Filters.Add(new GlobalExceptionAttribute());
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "RevCode_API/api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
